c97bc62e6406473a240ecf4492893439fbe1695d
ainewsto <113163264+ainewsto@users.noreply.github.com>
2025-10-20 17:01:05 +0800

    updated

--- Comfly.py
+++ Comfly.py
@@ -7537,7 +7537,8 @@ class Comfly_nano_banana_edit:
                 
                 data = {
                     "prompt": final_prompt,
-                    "model": model
+                    "model": model,
+                    "aspect_ratio": aspect_ratio 
                 }
                 
                 if response_format:
